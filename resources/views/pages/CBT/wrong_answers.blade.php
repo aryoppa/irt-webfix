939e1b4746285f4cd3b793cc0f97d6fc07a31e45
aryopamung <aryo.pamungkas@evermos.com>
2024-12-29 06:47:43 +0700

    done deal

--- resources/views/pages/CBT/wrong_answers.blade.php
+++ resources/views/pages/CBT/wrong_answers.blade.php
@@ -6,24 +6,30 @@
         @php
             $wrongAnswersCount = collect($results)->where('is_correct', false)->count();
         @endphp
-        <p class="text-dark text-center" style="font-size: 1.25rem; line-height: 1.8; margin-bottom: 2rem;">
-            Anda menjawab {{ $wrongAnswersCount }} soal dengan jawaban yang salah, berikut adalah pertanyaan yang gagal Anda jawab dengan benar:
-        </p>
-        <div class="text-dark" style="font-size: 1.125rem;">
-            <ul class="list-group">
-                @foreach ($results as $item)
-                    @if (!$item['is_correct'])
-                        <li class="list-group-item mb-3 shadow-sm">
-                            <strong>Soal:</strong> {{ $item['question_text'] }}<br>
-                            <strong>Jawaban Anda:</strong> <span class="text-danger">{{ $item['answer'] }}</span><br>
-                            <strong>Jawaban Benar:</strong> <span class="text-success">{{ $item['key_answer'] }}</span>
-                        </li>
-                    @endif
-                @endforeach
-            </ul>
-            <div class="text-center">
+        @if ($wrongAnswersCount == 0)
+            <p class="text-dark text-center" style="font-size: 1.25rem; line-height: 1.8; margin-bottom: 2rem;">
+                Kamu berhasil menjawab semua soal dengan benar. Untuk mengecek kemampuanmu, silahkan lakukan tes ulang.
+            </p>
+        @else
+            <p class="text-dark text-center" style="font-size: 1.25rem; line-height: 1.8; margin-bottom: 2rem;">
+                Anda menjawab {{ $wrongAnswersCount }} soal dengan jawaban yang salah, berikut adalah pertanyaan yang gagal Anda jawab dengan benar:
+            </p>
+            <div class="text-dark" style="font-size: 1.125rem;">
+                <ul class="list-group">
+                    @foreach ($results as $item)
+                        @if (!$item['is_correct'])
+                            <li class="list-group-item mb-3 shadow-sm">
+                                <strong>Soal:</strong> {{ $item['question_text'] }}<br>
+                                <strong>Jawaban Anda:</strong> <span class="text-danger">{{ $item['answer'] }}</span><br>
+                                <strong>Jawaban Benar:</strong> <span class="text-success">{{ $item['key_answer'] }}</span>
+                            </li>
+                        @endif
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div class="text-center">
             <a href="{{ url('irt/result') }}" class="btn btn-primary">Lihat Rekomendasi</a>
-        </div>
         </div>
     </div>
 </div>
